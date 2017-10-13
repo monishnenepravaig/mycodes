555c3283195a365357d829e2991df9e2bdd7c40c
Monish.Nene <monish.nene@colorado.edu>
2017-10-13 16:33:59 -0600

    adding structure hw4

---
+++ structq3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdint.h>
+
+struct timer
+{
+uint16_t TCTL;
+uint16_t TCCTL0;
+uint16_t TCCTL1;
+uint16_t TCCTL2;
+uint16_t TCCTL3;
+uint16_t TCCTL4;
+uint16_t TCCTL5;
+uint16_t TCCTL6;
+uint16_t TCR;
+uint16_t TCCR0;
+uint16_t TCCR1;
+uint16_t TCCR2;
+uint16_t TCCR3;
+uint16_t TCCR4;
+uint16_t TCCR5;
+uint16_t TCCR6;
+uint16_t TSTAT;
+uint16_t TIV0;
+uint16_t TIV1;
+uint16_t TIV2;
+uint16_t TIV3;
+uint16_t TIV4;
+uint16_t TIV5;
+uint16_t TIV6;
+}
