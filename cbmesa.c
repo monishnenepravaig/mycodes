efd2a47904e205e6eee2f674104200c6f1bfee9d
monishnene <31666297+monishnene@users.noreply.github.com>
2017-10-18 22:52:31 -0600

    Update cbmesa.c

--- cbmesa.c
+++ cbmesa.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-int count=0;
-int total=10.0;
-
+int count=0;  //global variable count
+int total=10; //total size of circular buffer
 
 float circularbuffer(int a, int* base)	
 {
@@ -9,27 +8,27 @@ int i;
 float average,sum=0;
 if(count==total-1)
 {
-	count=0;
+	count=0; //reset counter at end of the circular buffer
 }
 else
 {
-	count++;
+	count++; 
 }
 *(base + count)=a;
 for(i=0;i<10;i++)
 {
-sum = sum + *(base + i);
+sum = sum + *(base + i); //add all numbers in circular buffer
 }
-average=sum/total;
+average=sum/total; //average of numbers in circular buffer
 return average;
 }
 
 void main()
 {
-int cb[total],a;
+int cb[total],a; //declare circular buffer as an array with variable size
 float average;
 int* base=cb;
-for(a=0;a<total;a++)
+for(a=0;a<total;a++)//initialize circular buffer with 0 to avoid 
 {
 *(base+a)=0;
 }	
@@ -37,8 +36,9 @@ while(1)
 {
 printf("\n Enter the next number \n");
 scanf("%d",&a);
-average=circularbuffer(a,base);
+average=circularbuffer(a,base); //function call to circular buffer
+0000.
 printf("\n The new average = %f \n",average);
 }	
-}	
-	
+}
+
