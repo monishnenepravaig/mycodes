7e66cc7405fd44466a20b9028beaea3501a87172
Monish.Nene <monish.nene@colorado.edu>
2017-12-01 16:28:02 -0700

    Adding HWI Mesa

--- HWI.c
+++ HWI.c
@@ -31,183 +31,11 @@ void main()
 	uint8_t* c_ptr=&c;
 	uint8_t* x_ptr=&x;
 	uint8_t* y_ptr=&y;
-	uint8_t* z_ptr=&z;	
+	uint8_t* z_ptr=&z;
+	state = state_1R;	
 	while(1)
-	{
-		switch(state)
-		{
-			case state_1B:
-			printf("\n\rCurrent state is 1B");
-			{
-			if(a==0)
-			{
-				state=state_2R;
-			}
-			else
-			{
-				state=state_1R;
-			}	
-			break;
-			}
+	{	
 
-			case state_1R:
-			printf("\n\rCurrent state is 1R");
-			{
-			if(a==0)
-			{
-				state=state_1B;
-			}
-			else if (x==1)
-			{
-				state=state_1W;
-			}	
-			break;
-			}
-
-			case state_1W:
-			printf("\n\rCurrent state is 1W");
-			{
-			if(x==0)
-			{
-				state=state_1R;
-			}
-			else
-			{
-				state=state_2R;
-			}	
-			break;
-			}
-
-			case state_2B:
-			printf("\n\rCurrent state is 2B");
-			{
-			if(a==1)
-			{
-				state=state_1B;
-			}
-			else
-			{
-				if(b==1)
-				{
-					state=state_2R;
-				}
-				else
-				{
-					state=state_3R;
-				}
-			}	
-			break;
-			}
-
-			case state_2R:
-			printf("\n\rCurrent state is 2R");
-			{
-			if((a==1)&&(x==0))
-			{
-				state=state_1R;
-			}
-			else if (x==0)
-			{
-				state=state_1W;
-			}
-			else if (a==1)
-			{
-				state=state_1B;
-			}	
-			else if (b==0)
-			{
-				state=state_2B;
-			}
-			else if (y==1)
-			{
-				state=state_2W;
-			}
-			break;
-			}
-
-			case state_2W:
-			printf("\n\rCurrent state is 2W");
-			{
-			if(x==0)
-			{
-				state=state_1W;
-			}
-			else
-			{
-				if(y==0)
-				{	
-					state=state_2R;
-				}
-				else
-				{
-					state=state_3R; 					
-				}
-			}	
-			break;
-			}
-
-			case state_3B:
-			printf("\n\rCurrent state is 3B");
-			{
-			if(a==1)
-			{
-				state=state_1B;
-			}
-			else if(b==1)
-			{
-				state=state_2B;
-			}	
-			else if(c==1)
-			{
-				state=state_3R;
-			}	
-			break;
-			}
-
-			case state_3R:
-			printf("\n\rCurrent state is 3R");
-			{
-			if((b==1)&&(y==0))
-			{
-				state=state_2R;
-			}
-			else if (y==0)
-			{
-				state=state_2W;
-			}
-			else if (b==1)
-			{
-				state=state_2B;
-			}	
-			else if (c==0)
-			{
-				state=state_3B;
-			}
-			else if (z==1)
-			{
-				state=state_3W;
-			}
-			break;
-			}
-			
-			case state_3W:
-			printf("\n\rCurrent state is 3W");
-			{
-			if(x==0)
-			{
-				state=state_1W;
-			}
-			else if(y==0)
-			{
-				state=state_2W;
-			}	
-			else if(z==0)
-			{
-				state=state_3R;
-			}	
-			break;
-			}
-		}
 		printf("\n\r Enter next input");
 		while(1)		
 		{				
@@ -246,6 +74,241 @@ void main()
 		{
 			flip(z_ptr);
 			printf("\n\rvalue of z = %d",z);
+		}
+		switch(state)
+		{
+
+			case state_1B:
+			{
+			printf("\n\rcurrent state is 1B");
+			if(a==0)
+			{
+				state=state_2R;
+				printf("\n\rnext state is 2R");
+			}
+			else
+			{
+				state=state_1R;
+				printf("\n\rnext state is 1R");
+			}	
+			break;
+			}
+
+			case state_1R:
+			printf("\n\rCurrent state is 1R");
+			{
+			if(a==0)
+			{
+				state=state_1B;
+				printf("\n\rnext state is 1B");
+			}
+			else if (x==1)
+			{
+				state=state_1W;
+				printf("\n\rnext state is 1W");
+			}	
+			break;
+			}
+
+			case state_1W:
+			printf("\n\rCurrent state is 1W");
+			{
+			if(x==0)
+			{
+				state=state_1R;
+				printf("\n\rnext state is 1R");
+			}
+			else
+			{
+				state=state_2R;
+				printf("\n\rnext state is 2R");
+			}	
+			break;
+			}
+
+			case state_2B:
+			printf("\n\rCurrent state is 2B");
+			{
+			if(a==1)
+			{
+				state=state_1B;
+				printf("\n\rnext state is 1B");
+			}
+			else
+			{
+				if(b==1)
+				{
+					state=state_2R;
+					printf("\n\rnext state is 2R");
+				}
+				else
+				{
+					state=state_3R;
+					printf("\n\rnext state is 3R");
+				}
+			}	
+			break;
+			}
+
+			case state_2R:
+			printf("\n\rCurrent state is 2R");
+			{
+			if((a==1)&&(x==0))
+			{
+				state=state_1R;
+				printf("\n\rnext state is 1R");
+			}
+			else if (x==0)
+			{
+				state=state_1W;
+				printf("\n\rnext state is 1W");
+			}
+			else if (a==1)
+			{
+				state=state_1B;
+				printf("\n\rnext state is 1B");
+			}	
+			else if (b==0)
+			{
+				state=state_2B;
+				printf("\n\rnext state is 2B");
+			}
+			else if (y==1)
+			{
+				state=state_2W;
+				printf("\n\rnext state is 2W");
+			}
+			break;
+			}
+
+			case state_2W:
+			printf("\n\rCurrent state is 2W");
+			{
+			if(x==0)
+			{
+				state=state_1W;
+				printf("\n\rnext state is 1W");
+			}
+			else
+			{
+				if(y==0)
+				{	
+					state=state_2R;
+					printf("\n\rnext state is 2R");
+				}
+				else
+				{
+					state=state_3R;
+					printf("\n\rnext state is 3R");					
+				}
+			}	
+			break;
+			}
+
+			case state_3B:
+			printf("\n\rCurrent state is 3B");
+			{
+			if(a==1)
+			{
+				state=state_1B;
+				printf("\n\rnext state is 1B");
+			}
+			else if(b==1)
+			{
+				state=state_2B;
+				printf("\n\rnext state is 2B");
+			}	
+			else if(c==1)
+			{
+				state=state_3R;
+				printf("\n\rnext state is 3R");
+			}	
+			break;
+			}
+
+			case state_3R:
+			printf("\n\rCurrent state is 3R");
+			{
+			if((b==1)&&(y==0))
+			{
+				state=state_2R;
+				printf("\n\rnext state is 2R");
+			}
+			else if (y==0)
+			{
+				state=state_2W;
+				printf("\n\rnext state is 2W");
+			}
+			else if (b==1)
+			{
+				state=state_2B;
+				printf("\n\rnext state is 2B");
+			}	
+			else if (c==0)
+			{
+				state=state_3B;
+				printf("\n\rnext state is 3B");
+			}
+			else if (z==1)
+			{
+				state=state_3W;
+				printf("\n\rnext state is 3W");
+			}
+			break;
+			}
+			
+			case state_3W:
+			printf("\n\rCurrent state is 3W");
+			{
+			if(x==0)
+			{
+				state=state_1W;
+				printf("\n\rnext state is 1W");
+			}
+			else if(y==0)
+			{
+				state=state_2W;
+				printf("\n\rnext state is 2W");
+			}	
+			else if(z==0)
+			{
+				state=state_3R;
+				printf("\n\rnext state is 3R");
+			}	
+			break;
+			}
+
+/*			
+			case state_3R:			
+			{
+				printf("\n\rCurrent state is 3R");
+				if((z==1)||(c==0))
+				{
+					state=state_1R;
+					printf("\n\rnext state is 1R");
+				}
+				break;
+			}
+			
+			case state_2R:
+			{
+				printf("\n\rCurrent state is 2R");
+				state=state_3R;
+				printf("\n\rnext state is 3R");
+				break;
+			}
+
+			case state_1R:
+			{
+				printf("\n\rCurrent state is 1R");
+				if((a==0)||(x==1))
+				{
+					state=state_2R;
+					printf("\n\rnext state is 2R");
+				}	
+				break;
+			}
+*/
 		}
 	}
 }
