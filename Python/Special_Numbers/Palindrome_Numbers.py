345371d1f97a167bb97277aad0dcaa95a0ef8818
monishnene <31666297+monishnene@users.noreply.github.com>
2018-06-19 23:18:52 -0600

    Palindrome done

--- Python/Special_Numbers/Palindrome_Numbers.py
+++ Python/Special_Numbers/Palindrome_Numbers.py
@@ -1,6 +1,7 @@
-print("Ugly Numbers -> Numbers with only -1, 2, 3 and 5 as prime factors.")
+from Number_Operations import digits
+print("Palindrome Numbers -> A number that is the same written forwards or backwards")
 condition = True
-while(condition):
+while True:
     string = input("Enter next number to be checked ")
     if string == "":
         break
@@ -9,19 +10,11 @@ while(condition):
     except:
         print("Invalid Input")
         continue
-    if number < 0:
-        temp = -1 * number
+    split = digits(number)
+    split_2 = split[::-1]
+    if split == split_2:
+        print(number,"is a palindrome number.")
     else:
-        temp = number
-    while (temp % 2 == 0) and ( temp >= 2 ):
-        temp /= 2
-    while (temp % 3 == 0) and ( temp >= 3 ):
-        temp /= 3
-    while (temp % 5 == 0) and ( temp >= 5 ):
-        temp /= 5
-    if temp == 1 or temp == 0 :
-        print(number,"is an ugly number.")
-    else:
-        print(number,"is a beautiful number.")
-print("Ugly numbers Code Ended")
+        print(number,"is not a palindrome number.")
+print("Palindrome numbers Code Ended")
 
