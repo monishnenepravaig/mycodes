fec3fd244b58f30aba6b81c8dd1e822a803db5d9
Monish Nene <monish.nene@colorado.edu>
2018-07-13 12:20:05 -0600

    Update array_size.sh

--- Shell/Hackerrank_challenges/array_size.sh
+++ Shell/Hackerrank_challenges/array_size.sh
@@ -1,15 +1,6 @@
-j=0
 array=()
-while [ $j -lt 20 ]
+while read a;
 do
-    read a
-    if [[ "${a}" == *"N"* ]];then
-    array+=("$a") 
-    elif [[ "${a}" == *"A"* ]];then
-    array+=("$a") 
-    elif [[ "${a}" == *"G"* ]];then
-    array+=("$a") 
-    fi
-    ((j++))
+    array+=("$a")
 done
-echo ${#array[*]}
+echo ${#array[*]} 
