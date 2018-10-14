4d2c3d9659d0019399c2e8a51be6305db590a283
monishnene <31666297+monishnene@users.noreply.github.com>
2018-10-14 01:15:03 -0600

    Update C/Hackerrank_challenges/Solution_Functions.c

--- C/Hackerrank_challenges/Solution_Functions.c
+++ C/Hackerrank_challenges/Solution_Functions.c
@@ -11,17 +11,6 @@
 char* readline();
 char** split_string(char*);
 
-/***********************************************************************
- * rotLeft()
- * Author: Monish Nene
- * Date: 09/28/2018
- * @brief Rotating an array n number of times
- * @param a_count size of array
- * @param a array pointer
- * @param d number of rotations
- * @param result_count pointer to return size of result array
- * @return maximum sum of hourglass shape in the given matrix
-***********************************************************************/
 int* rotLeft(int a_count, int* a, int d, int* result_count)
 {
     int j=0;
@@ -47,16 +36,6 @@ int* rotLeft(int a_count, int* a, int d, int* result_count)
     return a;
 }
 
-/***********************************************************************
- * hourglassSum()
- * Author: Monish Nene
- * Date: 09/26/2018
- * @brief Finding the maximum sum of hourglass shape in the given matrix
- * @param arr_rows no. of rows
- * @param arr_columns no. of columns
- * @param arr data pointer
- * @return maximum sum of hourglass shape in the given matrix
-***********************************************************************/
 int hourglassSum(int arr_rows, int arr_columns, int** arr)
 {
     int sum=0,max_sum=0x80000001,row=0,column=0;
@@ -72,4 +51,107 @@ int hourglassSum(int arr_rows, int arr_columns, int** arr)
         }
     }
     return max_sum;
+}
+
+// Complete the flippingBits function below.
+long flippingBits(long n)
+{
+    long a=0xFFFFFFFF;
+    n=a-n;
+    printf("%ld",n);
+    return n;
+}
+
+char* primality(int n)
+{
+    char* np= "Not prime";
+    char* p="Prime";
+    int i=2;
+    if(n==1)
+    {
+        return np;
+    }
+    while(i*i<=n)
+    {
+        if(n%i==0)
+        {
+            return np;
+        }
+        i++;
+    }
+    return p;
+}
+
+void checkMagazine(int magazine_count, char** magazine, int note_count, char** note)
+{
+    int i=0,j=0,k=0,matched=0,not_matched=0,not_matched_flag=1,matched_flag=0,len=0;
+    char* empty="";
+    for(i=0;i<note_count;i++)
+    {
+        not_matched_flag=1;
+        len=strlen(*(note+i));
+        for(j=0;j<magazine_count;j++)
+        {
+            k=len;
+            matched_flag=1;
+            while((*(*(note+i)+k)!=0)||(*(*(magazine+j)+k)!=0)||(k>=0))
+            {
+                if((*(*(note+i)+k)!=*(*(magazine+j)+k)))
+                {
+                    matched_flag=0;
+                    break;
+                }
+                k--;
+            }
+            if(matched_flag)
+            {
+                matched++;
+                not_matched_flag=0;
+                *(magazine+j)=empty;
+                break;
+            }
+        }
+        if(not_matched_flag)
+        {
+            not_matched++;
+        }
+        if((i+not_matched>magazine_count)||(not_matched>magazine_count-note_count))
+        {
+            break;
+        }
+    }
+    if(matched==note_count)
+    {
+        printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+}
+
+char* twoStrings(char* s1, char* s2)
+{
+    char* yes="YES";
+    char* no="NO";
+    char ascii[256];
+    uint16_t i=0;
+    for(i=0;i<256;i++)
+    {
+        ascii[i]=0;
+    }
+    while(*s1!=0)
+    {
+        ascii[*s1++]=1;
+        s1++;
+    }
+    while(*s2!=0)
+    {
+        if(ascii[*s2]==1)
+        {
+            return yes;
+        }
+        s2++;
+    }
+    return no;
 }
