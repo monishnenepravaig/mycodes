abbc8616a3fe6287c5611ad8cfec6f06dd326dbc
Monish Nene <monish.nene@colorado.edu>
2021-07-29 15:25:40 +0530

    Update main.h

--- C/data_structures/main.h
+++ C/data_structures/main.h
@@ -1,8 +1,2 @@
 #include "common.h"
 #include "linklist.h"
-#include "file_operations.h"
-#define TOTAL_POKEMON 718
-#define WORDSIZE 20
-#define ALPHABETS 26
-
-static uint8_t* fname="pokemon_raw.txt";
